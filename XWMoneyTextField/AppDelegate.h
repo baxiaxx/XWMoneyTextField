426869ee65f03b293e36b577873a2796385c2ad4
Zhang Xiaowei <weixiao08@qq.com>
2016-03-10 22:57:01 +0800

    Initial Commit

---
+++ XWMoneyTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XWMoneyTextField
+//
+//  Created by smile.zhang on 16/3/10.
+//  Copyright © 2016年 smile.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XWMoneyTextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XWMoneyTextField
+//
+//  Created by smile.zhang on 16/3/10.
+//  Copyright © 2016年 smile.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
